3aadcdd9098bf28b406acf8fc1c455efdce15029
dongyun <0902hdy@gmail.com>
2024-03-19 15:46:20 +0900

    initial commit

---
+++ src/main/java/com/capstone/learning_squad_be/LearningSquadBeApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.learning_squad_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSquadBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSquadBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/learning_squad_be/LearningSquadBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.learning_squad_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSquadBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
