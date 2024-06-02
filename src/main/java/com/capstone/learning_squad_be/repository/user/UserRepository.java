9f9dcb15621adcd841d87a8ddc090011ff48516f
dongyun <0902hdy@gmail.com>
2024-06-02 22:52:35 +0900

    fix:userOAuth-nickname

--- src/main/java/com/capstone/learning_squad_be/repository/user/UserRepository.java
+++ src/main/java/com/capstone/learning_squad_be/repository/user/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUserName(String userName);
-    Optional<User> findByNickName(String nickName);
-    boolean existsByNickName(String nickName);
+//    Optional<User> findByNickName(String nickName);
+//    boolean existsByNickName(String nickName);
 }
